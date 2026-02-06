948ea6a849b6c484a5d215bf3e00013fce529882
astryhs <superushatova@gmail.com>
2026-02-06 20:04:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import { formatDateToRU, formatDateToUS } from "./lib/formatDate/formatDate.js";
+
+let country = "ru";
+
+const formatDateForCountry = (date, country) => {
+  switch (country) {
+    case "us":
+      return formatDateToUS(date);
+    case "ru":
+      return formatDateToRU(date);
+    default:
+      return formatDateToRU(date);
+  }
+};
+const currentDate = new Date();
+const now = formatDateForCountry(currentDate, country);
+
+const weekLater = new Date(currentDate.getTime() + 1000 * 60 * 60 * 24 * 7);
+const then = formatDateForCountry(weekLater, country);
+
+document.getElementById("now").innerHTML = now;
+document.getElementById("then").innerHTML = then;
